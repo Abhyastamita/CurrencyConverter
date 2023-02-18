cf62cfba2b3dfae516f3b6eafa40b1eb3d052932
Katherine Perdue <krp0047@auburn.edu>
2023-02-18 16:10:50 -0500

    Added ConverterLogic functions.

--- CurrencyConverter/Model/ConverterLogic.swift
+++ CurrencyConverter/Model/ConverterLogic.swift
@@ -8,5 +8,43 @@
 import Foundation
 
 struct ConverterLogic {
+    var usd : Int = 0
+    var euroBool : Bool = true
+    var realBool : Bool = true
+    var bahtBool : Bool = true
+    var yuanBool : Bool = true
     
+    mutating func setSwitch(_ switchValue : Bool, _ currency : inout Bool) {
+        if switchValue {
+            currency = true
+        } else {
+            currency = false
+        }
+    }
+    
+    mutating func getEuroValue() -> Double {
+        return Double(usd) * 0.93
+    }
+    
+    mutating func getRealValue() -> Double {
+        return Double(usd) * 5.17
+    }
+    
+    mutating func getBahtValue() -> Double {
+        return Double(usd) * 34.45
+    }
+    
+    mutating func getYuanValue() -> Double {
+        return Double(usd) * 6.87
+    }
+    
+    mutating func getRequestedConversions() -> Dictionary<String, Double> {
+        var converted = [ "euro": euroBool ? getEuroValue() : -1.0,
+                          "real": realBool ? getRealValue() : -1.0,
+                          "baht": bahtBool ? getBahtValue() : -1.0,
+                          "yuan": yuanBool ? getYuanValue() : -1.0
+        ]
+        return converted
+    }
+
 }
